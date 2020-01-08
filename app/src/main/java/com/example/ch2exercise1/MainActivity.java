7f2cbc947fc408e97d19cc62c59ab8648fddd2e6
bagasmad <bagas.m@mail.ugm.ac.id>
2020-01-08 08:21:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ch2exercise1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ch2exercise1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view)
+    {
+        Log.i("Information","Button is Clicked");
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
